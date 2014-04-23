05a4b63f4a702be3c633eed871f1166b747ea327
Raphael Fabeni <rapha.fabeni@gmail.com>
2014-04-23 01:18:18 -0300

    Removing folder assets/img and some dev dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,73 +16,8 @@ module.exports = function(grunt) {
 
             // Projects assets and build paths for .js files
             dev_js: '_assets/js',
-            build_js: 'build/js',
+            build_js: 'build/js'
 
-            // Projects assets and build paths for img files
-            dev_img: '_assets/img',
-            build_img: 'build/img'
-        },
-
-        // -- Image min --------------------------------------------------------------
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= globalConfig.dev_img %>',
-                    src:['*.{jpg,gif}'],
-                    dest: '<%= globalConfig.build_img %>',
-                }],
-                options: {
-                    cache: false
-                }
-            }
-        },
-
-        // -- SVG min -------------------------------------------------------------------
-        svgmin: {
-            options: {
-                plugins: [{
-                    removeViewBox: false
-                }, {
-                    removeUselessStrokeAndFill: false
-                }, {
-                    convertPathData: { 
-                        straightCurves: false
-                    }
-                }]
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= globalConfig.dev_img %>',
-                    src: ['*.svg'],
-                    dest: '<%= globalConfig.build_img %>',
-                    ext: '.svg'
-                }]
-            }
-        },
-
-        // -- SVG 2 PNG ----------------------------------------------------------------
-        svg2png: {
-            all: {
-                files: [{
-                    src: ['<%= globalConfig.dev_img %>/*.svg'],
-                    dest: '<%= globalConfig.build_img %>'
-                }]
-            }
-        },
-
-        // -- Copy img config -----------------------------------------------------------
-        copy: {
-            main: {
-                files: [{
-                    expand: true,
-                    flatten: true,
-                    src: ['<%= globalConfig.dev_img %>/*.png'],
-                    dest: '<%= globalConfig.build_img %>',
-                    filter: 'isFile'
-                }]
-            }
         },
 
         // -- Concat config ----------------------------------------------------------
@@ -160,40 +95,6 @@ module.exports = function(grunt) {
                 ]
             },
 
-            // -- Images config -------------------------------------------------------
-            images: {
-                files: [
-                    '<%= globalConfig.dev_img %>/*.gif',
-                    '<%= globalConfig.dev_img %>/*.jpg'
-                ],
-                tasks: ['imagemin'],
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            },
-
-            copy: {
-                files: [
-                    '<%= globalConfig.dev_img %>/*.png'
-                ],
-                tasks: ['copy'],
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            },
-
-            // -- SVG config ----------------------------------------------------------
-            svg: {
-                files: ['<%= globalConfig.dev_img %>/*.svg'],
-                tasks: ['svgmin', 'svg2png'],
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            },
-
             // -- Project files config (CSS and JS) ------------------------------------
             scripts_dev: {
                 files: ['<%= concat.dev.src %>'],
@@ -223,10 +124,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-svgmin');
-    grunt.loadNpmTasks('grunt-svg2png');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['watch', 'concat', 'uglify', 'jshint', 'sass', 'imagemin', 'svgmin', 'svg2png', 'copy'] );
@@ -242,9 +139,6 @@ module.exports = function(grunt) {
 
     // JS task
     grunt.registerTask( 'js', [ 'concat', 'uglify', 'jshint' ] );
-
-    // Compress images task
-    grunt.registerTask( 'images', [ 'svgmin', 'svg2png', 'copy', 'imagemin' ] );
 
     // Start taks
     grunt.registerTask( 'init', [ 'uglify:modernizr'] );
